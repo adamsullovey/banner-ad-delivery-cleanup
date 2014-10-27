6d59635ef611b4086287d6db0d4c37adc9a9bb19
Adam Sullovey <chilly@oddlystudios.com>
2014-10-27 12:54:19 -0400

    adding actual script

---
+++ delivery-cleanup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+# this script copies the working folders and removes files not needed for final delivery
+
+import os
+import sys
+import shutil
+
+def scandirs(path):
+
+	# this will be the new folder that working files are copied to
+	deliverables_path = os.path.join(path, 'deliverables')
+
+	# try to delete it if it exists already
+	try:
+		shutil.rmtree(deliverables_path)
+
+	except FileNotFoundError:
+		print("could not delete " + deliverables_path)
+
+	# copy working files
+	shutil.copytree(path, deliverables_path);
+
+	print("copied to " + deliverables_path)
+
+	# now delete junk
+	for root, dirs, files in os.walk(deliverables_path):
+
+		for currentFile in files:
+			exts=('.swf', '.jpg', '.png')
+			if not any(currentFile.lower().endswith(ext) for ext in exts):
+				path = os.path.join(root, currentFile)
+				print("delete file: " + path)
+				os.remove(path)
+
+		for currentDir in dirs:
+			if "assets" in currentDir:
+				print("delete dir: " + currentDir)
+				shutil.rmtree(os.path.join(root, currentDir))
+
+
+scandirs('./')
